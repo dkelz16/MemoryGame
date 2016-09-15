36669b52135202bb7bd8791f86280fdc9e477ca4
Allen Hazlett <allendwighthazlett@yahoo.com>
2016-09-15 08:17:13 -0400

    Put Dave printBoard to dave.h

--- Memory_Game/Memory_Game/Memory_Game.cpp
+++ Memory_Game/Memory_Game/Memory_Game.cpp
@@ -34,63 +34,7 @@ void createFullArray()
 
 }
 
-void displayNumberedBoard(int columns, int rows) //DISPLAYS THE BOARD WITH NUMBERS (UP TO 100)
-{
-	int count = 1;
-	
-	for (int i = 0; i < columns; i++)
-	{
-		cout << "|";
-
-		for (int j = 0; j < rows; j++)
-		{
-			cout << "- - - - -|";
-		}
-
-		cout << "\n";
-
-		for (int s = 0; s < 3; s++)
-		{
-			for (int r = 0; r < rows; r++)
-			{
-				if (s == 1)
-				{
-					if (count <= 9)
-					{
-						cout << "|    " << count << "    ";
-						count++;
-					}
-					else if((count > 9) && (count < 100))
-					{
-						cout << "|   " << count << "    ";
-						count++;
-					}
-					else
-					{
-						cout << "|  " << count << "    ";
-						count++;
-					}
-					
-				}
-				else
-				{
-					cout << "|         ";
-				}
-				
-			}
-			
-			cout << "|\n";	
-		}	
-	}
-	
-	for (int p = 0; p < rows; p++)
-	{
-		cout << "|- - - - -";
-	}
-
-	cout << "|";
-	cout << "\n\n";
-}
+//ADDED TO DAVE.h
 
 void printGoodbyeMessage()
 {
