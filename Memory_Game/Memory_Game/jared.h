5a0d3f63c263483291e7f97d24ecb5ff142bc4bf
Allen Hazlett <ahazlett24@southhills.edu>
2016-09-15 10:35:39 -0400

    Added getNumPairs() to jared.h

--- Memory_Game/Memory_Game/jared.h
+++ Memory_Game/Memory_Game/jared.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "dave.h"
+#include "allen.h"
+#include "derek.h"
+
+int getNumPairs()
+{
+	int pairs = 0;
+
+	cout << "please enter the ammount of pairs you would like: " << endl;
+	cin >> pairs;
+	while (pairs >= 51 || pairs <= 1)
+	{
+		cout << "please put in a different number the one you entered was greater then 50 or less than 2" << endl;
+		cout << "please enter the ammount of pairs you would like: " << endl;
+		cin >> pairs;
+	}
+	cout << pairs << endl;
+	return pairs;
+}
