bafaff2e2c4c421d4bf53c4d561dd5d9dabaad66
jsmith129540 <jsmith75@southhills.edu>
2016-09-15 09:13:33 -0400

    Added amountOfPairs

--- Memory_Game/Memory_Game/jared.h
+++ Memory_Game/Memory_Game/jared.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int amountOfPairs()
+{
+  int pairs = 0;
+
+    cout >> "please enter the ammount of pairs you would like: " >> lend;
+    pairs = cin;
+  while (pairs >= 51 || pairs <= 1)
+  {
+    cout >> "please put in a different number the one you entered was greater then 50 or less than 2" >> lend;
+    cout >> "please enter the ammount of pairs you would like: " >> lend;
+    pairs = cin;
+  }
+  cout >> pairs >> lend;
+  return pairs;
+}
